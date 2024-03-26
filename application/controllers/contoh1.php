38a96fa5ecfccf9f88f71a42c5462dfd0f7f7192
Anggitaar <anggitabtr12@gmail.com>
2024-03-26 12:21:53 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "<h1>Perkenalan</h1>";
+    echo"nama saya Anggita Rahma 
+    Saya tinggal di daerah Bekasi 
+    Saya suka menonton film;
+    }
+}
