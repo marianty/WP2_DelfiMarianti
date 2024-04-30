ec37951f9bd09ab8889779874a17b84a1acbb275
marianty <delfimarianty@gmai.com>
2024-04-30 12:11:47 +0700

    [ADD] Pengerjaan Pertemuan 2

---
+++ application/views/v_about.php
@@ -0,0 +1,26 @@
+<head>
+<link rel="stylesheet" type="text/css" 
+href="http://localhost/pustaka-booking/assets/css/stylebuku.css">
+</head>
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4> Nama</h4>
+    <li> Nama Depan : DELFI MARIANTI</li>
+    <li> Nama Belakang : MANALU</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+    <li> RAWALUMBU BEKASI</li>
+</ul>
+
+<h4>Tempat Lahir</h4>
+<ul type="none">
+    <li>Pagaran Pinasa</li>
+    </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+</ul>
+</section>
+ 
